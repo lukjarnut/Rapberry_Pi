2a0339a9ca634218d333d956ccac759f49af08b5
XRamaR <luk.jarnut@gmail.com>
2020-03-29 15:19:56 +0200

    Update joy.py

--- sense_hat/joy.py
+++ sense_hat/joy.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 from sense_emu import SenseHat
-from getch import getch
+import getch
 from time import sleep
 import threading
 import json
@@ -11,42 +11,67 @@ sense = SenseHat()
 char = None
 x_pos = 0
 y_pos = 0
-button_p = 0
-#bool_1 = True #Program status on/off
+button_c = 0
+stop = False #Program status on/off
 
+def observer(): #function waiting for char and stopping read and write
+    try:
+        None
+    except BaseException:
+       reader.join()
+       writer.join()
+       sys.exit(0)
+    
 def read():
     while True:
+        try:
+          global stop
           global x_pos
           global y_pos
-          global button_p
+          global button_c
           for event in sense.stick.get_events():
               if event.action == "pressed":
                 if event.direction == "up":
                     y_pos += 1
-                    #print("y", y_pos)
                 if event.direction == "down":
                     y_pos -= 1
-                    #print("y", y_pos)
                 if event.direction == "left":
                     x_pos -= 1
-                    #print("x", x_pos)
                 if event.direction == "right":
                     x_pos += 1
-                    #print("x", x_pos)
                 if event.direction == "middle":
-                    button_p += 1
+                    button_c += 1
+          
           sleep(0.5)
+        except (KeyboardInterrupt, SystemExit):
+            reader.join()
+            writer.join()
+            sys.exit(0)
 def write():
     while True:
+       global stop
        global x_pos
        global y_pos
-       global button_p
-       json_exit = json.dumps([{'x':x_pos},{'y':y_pos},{'b':button_p}])
+       global button_c
+       json_exit = json.dumps([{'x':x_pos},{'y':y_pos},{'b':button_c}])
+       fil = open('joy_data.dat', 'a')
+       fil.write(json_exit)
+       fil.write(',')
        print(json_exit)
        sleep(1)
-
+        
 reader = threading.Thread(target=read)
 writer = threading.Thread(target=write)
 
+fil = open('joy_data.dat', 'w')
+fil.write('[')
+fil.write(']')
+
 reader.start()
 writer.start()
+
+
+# char = getch.getch()
+# if char != None:
+#   status = True
+#   break
