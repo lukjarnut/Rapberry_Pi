618b4076381a708fda34603b9ef6a68a9266f176
XRamaR <luk.jarnut@gmail.com>
2020-03-30 23:03:19 +0200

    auto SH screen rotate +new HTML

--- sense_hat/diode_lit.php
+++ sense_hat/diode_lit.php
@@ -1,18 +1,20 @@
 <?php
 
-if(empty($_GET))
-{
-	echo shell_exec("./led_clear.py 2>&1");
-}
+//print_r($_POST);
+
+shell_exec("./led_clear.py 2>&1");
+
 for ($i = 0; $i <= 7; $i++) 
 {
     for ($j = 0; $j <= 7; $j++) 
 	{
-		if($_GET["$i$j"])
+		if($_POST["$i$j"] == "r" || $_POST["$i$j"] == "g" || $_POST["$i$j"] == "b")
 		{
-			echo shell_exec("./led.py -x $i -y $j 2>&1");
+		$color = $_POST["$i$j"];
+		shell_exec("./led.py -x $i -y $j -c $color 2>&1");
 		}
 	}
-}
+} 
+shell_exec("./screen_rot.py 2>&1");
 header("Location: /sensehat/diode.html");
 ?>
